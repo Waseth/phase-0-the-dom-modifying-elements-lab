cdc7944a1d46fa0cbf2086fd86ac04ecd3e17949
Waseth <waseth.emmanuel@student.moringaschool.com>
2024-04-01 12:11:19 +0300

    creatednewfile.md

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.querySelector('#main');
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Your Name is the champion';
+newHeader.textContent = newHeader.textContent.replace('Your Name', 'Samuel'); // Replace this with your name
+
+document.body.appendChild(newHeader);
